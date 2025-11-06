543655d763a5707553f38f161efdf0479f7eb261
ritikdevelop <hrithikrana216@gmail.com>
2025-11-06 10:04:34 +0530

    chore: standardize quotation marks and formatting in Routes.ts

--- src/navigation/Routes.ts
+++ src/navigation/Routes.ts
@@ -1,51 +1,49 @@
-
-import { NavigatorScreenParams } from '@react-navigation/native';
+import { NavigatorScreenParams } from "@react-navigation/native";
 
 export enum RootRoutes {
-    AuthStack = 'AuthStack',
-    MainTabs = 'MainTabs',
+  AuthStack = "AuthStack",
+  MainTabs = "MainTabs",
 }
 
 export enum AuthRoutes {
-    Login = 'Login',
-    SignUp = 'SignUp',
+  Login = "Login",
+  SignUp = "SignUp",
 }
 
 export enum MainRoutes {
-    Home = 'Home',
-    Store = 'Store',
-    ProductDetails = 'ProductDetails',
-    Profile = 'Profile',
-    Checkout = 'Checkout',
-    Category = 'Category',
-    Cart = 'Cart',
-    AddAddress = 'AddAddress',
-    EditAddress = 'EditAddress',
+  Home = "Home",
+  Store = "Store",
+  ProductDetails = "ProductDetails",
+  Profile = "Profile",
+  Checkout = "Checkout",
+  Category = "Category",
+  Cart = "Cart",
+  AddAddress = "AddAddress",
+  EditAddress = "EditAddress",
 }
-
 
 export type RootStackParamList = {
-    [RootRoutes.AuthStack]: undefined;
-    [RootRoutes.MainTabs]: undefined;
-}
+  [RootRoutes.AuthStack]: undefined;
+  [RootRoutes.MainTabs]: undefined;
+};
 
 export type AuthStackParamList = {
-    [AuthRoutes.Login]: undefined;
-    [AuthRoutes.SignUp]: undefined;
-}
+  [AuthRoutes.Login]: undefined;
+  [AuthRoutes.SignUp]: undefined;
+};
 
 export type MainTabParamList = {
-    [MainRoutes.Home]: undefined;
-    [MainRoutes.Store]: undefined;
-    [MainRoutes.Cart]: undefined;
-}
+  [MainRoutes.Home]: undefined;
+  [MainRoutes.Store]: undefined;
+  [MainRoutes.Cart]: undefined;
+};
 
 export type MainStackParamList = {
-    MainTabs:NavigatorScreenParams<MainTabParamList>;
-    [MainRoutes.ProductDetails]: { productId: string };
-    [MainRoutes.Profile]: undefined;
-    [MainRoutes.Checkout]: undefined;
-    [MainRoutes.Category]: undefined;
-    [MainRoutes.AddAddress]: undefined;
-    [MainRoutes.EditAddress]: undefined;
-}
+  MainTabs: NavigatorScreenParams<MainTabParamList>;
+  [MainRoutes.ProductDetails]: { productId: string };
+  [MainRoutes.Profile]: undefined;
+  [MainRoutes.Checkout]: undefined;
+  [MainRoutes.Category]: undefined;
+  [MainRoutes.AddAddress]: undefined;
+  [MainRoutes.EditAddress]: undefined;
+};
